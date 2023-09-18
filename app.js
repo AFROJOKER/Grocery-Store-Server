f496926ef15b0d5d78f9efc5bffdbd95cbde4477
AFROJOKER <maximgav123@gmail.com>
2023-09-18 16:50:40 +0300

    Added comments and public folder

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
+//.env file configuration
 require("dotenv").config();
+
+//Required Imports
 const express = require("express");
+const path = require("path");   
 const cors = require('cors');
 const app = express();
 const userRoute = require('./routes/users');
 const productRoute = require('./routes/products');
+
+//Running a file that connects to mongodb
 require("./db/mongoConnect");
 
+//Middlewares
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors());
 app.use(express.json());
 
+//Setting up the routes
 app.get("/",(req,res)=>{res.json("hi")})
 app.use("/users",userRoute);
 app.use("/products",productRoute);
 
-
+//Running the server
 const port = process.env.PORT || 3001;
 app.listen(port);
